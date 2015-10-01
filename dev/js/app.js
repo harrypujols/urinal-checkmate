9f1e4331569c5f212bb54306cfc2a3697f210762
Harry Pujols <harry.pujols@pgmmb.com>
2015-10-01 13:08:11 -0400

    so close...

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,18 +1,34 @@
 console.log('app is running');
 
-$('.urinal').load('/img/urinal.svg');
 $('.man').load('/img/man.svg');
+
+// function allowDrop(ev) {
+//   ev.preventDefault();
+// }
+//
+// function drag(ev) {
+//   ev.dataTransfer.setData('text', ev.target.id);
+// }
+//
+// function drop(ev) {
+//   ev.preventDefault();
+//   var data = ev.dataTransfer.getData('text');
+//   ev.target.appendChild(document.getElementById(data));
+// }
+
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+  ev.dataTransfer.setData('text/html', ev.target.id);
 }
 
-function drop(ev) {
+function drop(ev, target) {
   ev.preventDefault();
-  var data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
+  console.log(target.id, ev.target.id)
+
+  var data = ev.dataTransfer.getData('text/html');
+  alert(data)
 }
