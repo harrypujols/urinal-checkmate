4e34f7a9110d51cfea33a15ab8977084027e2f54
Harry Pujols <harry.pujols@pgmmb.com>
2015-10-01 13:11:46 -0400

    ok

--- dev/js/app.js
+++ dev/js/app.js
@@ -2,33 +2,20 @@ console.log('app is running');
 
 $('.man').load('/img/man.svg');
 
-// function allowDrop(ev) {
-//   ev.preventDefault();
-// }
-//
-// function drag(ev) {
-//   ev.dataTransfer.setData('text', ev.target.id);
-// }
-//
-// function drop(ev) {
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData('text');
-//   ev.target.appendChild(document.getElementById(data));
-// }
-
 
 function allowDrop(ev) {
   ev.preventDefault();
 }
 
 function drag(ev) {
-  ev.dataTransfer.setData('text/html', ev.target.id);
+  ev.dataTransfer.setData('text', ev.target.id);
 }
 
 function drop(ev, target) {
   ev.preventDefault();
   console.log(target.id, ev.target.id)
 
-  var data = ev.dataTransfer.getData('text/html');
-  alert(data)
+  var data = ev.dataTransfer.getData('text');
+
+  ev.target.appendChild(document.getElementById(data));
 }
