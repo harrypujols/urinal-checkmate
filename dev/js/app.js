c93385321516c649b822625855f12f45e1531560
Harry Pujols <harry.pujols@pgmmb.com>
2015-10-01 12:24:35 -0400

    native dragdrop

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,7 +1,18 @@
-(function() {
 console.log('app is running');
 
 $('.urinal').load('/img/urinal.svg');
 $('.man').load('/img/man.svg');
 
-}).call(this);
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
