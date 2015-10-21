f63b49cab9a65b434dd2153c37859f44bbc83421
Harry Pujols <harry.pujols@pgmmb.com>
2015-10-21 09:54:22 -0400

    native angular dragdrop

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,28 +1,98 @@
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData('text', ev.target.id);
-}
-
-function drop(ev, target) {
-  ev.preventDefault();
-  var data = ev.dataTransfer.getData('text');
-  ev.target.appendChild(document.getElementById(data));
-  checkMate(target.id);
-}
-
-function checkMate(el) {
-  if (el == 'correct') {
-    console.log('correct!');
-  } else {
-    console.log('nothing');
-  }
-}
-
 
 var app = angular.module('urinal-chess', []);
+
+app.directive('draggable', function() {
+  return function(scope, element) {
+    var el = element[0];
+
+    el.draggable = true;
+
+    el.addEventListener(
+      'dragstart',
+      function(e) {
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('Text', this.id);
+        this.classList.add('drag');
+        return false;
+      },
+      false
+    );
+
+    el.addEventListener(
+      'dragend',
+      function(e) {
+        this.classList.remove('drag');
+        return false;
+      },
+      false
+    );
+  }
+});
+
+
+
+app.directive('droppable', function() {
+  return {
+    scope: {
+      drop: '&',
+      bin: '='
+    },
+    link: function(scope, element) {
+      var el = element[0];
+
+      el.addEventListener(
+        'dragover',
+        function(e) {
+          e.dataTransfer.dropEffect = 'move';
+          if (e.preventDefault) e.preventDefault();
+          this.classList.add('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragenter',
+        function(e) {
+          this.classList.add('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragleave',
+        function(e) {
+          this.classList.remove('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'drop',
+        function(e) {
+          if (e.stopPropagation) e.stopPropagation();
+
+          this.classList.remove('over');
+
+          var binId = this.id;
+          var item = document.getElementById(e.dataTransfer.getData('Text'));
+          this.appendChild(item);
+          scope.$apply(function(scope) {
+            var fn = scope.drop();
+            if ('undefined' !== typeof fn) {
+              fn(item.id, binId);
+            }
+          });
+
+          return false;
+        },
+        false
+      );
+    }
+  }
+});
 
 app.controller('uctrl', function($scope, $http) {
 
@@ -42,5 +112,9 @@ app.controller('uctrl', function($scope, $http) {
     for (var i = min; i <= max; i += step) input.push(i);
     return input;
   };
+
+  $scope.handleDrop = function(item, bin) {
+    alert('Item ' + item + ' has been dropped into ' + bin);
+  }
 
 });
