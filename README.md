92ff1b92a52abb578feb54adbe1092800be99b25
Harry Pujols <harry.pujols@pgmmb.com>
2015-10-22 13:29:47 -0400

    Update 2015-10-22T17:29:45.484Z

--- README.md
+++
@@ -1,8 +0,0 @@
-URINAL CHESS
-============
-
-[![David Link](https://david-dm.org/harrypujols/gulp.png)](https://david-dm.org/harrypujols/urinal-chess)
-
-A game where your number one priority is Number One.
-
-Coming soon.
---
+++ js/app.js
@@ -0,0 +1,154 @@
+
+var app = angular.module('urinal-chess', []);
+
+// drag
+app.directive('draggable', function() {
+  return function(scope, element) {
+    var el = element[0];
+
+    el.draggable = true;
+
+    el.addEventListener(
+      'dragstart',
+      function(e) {
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('Text', this.id);
+        this.classList.add('drag');
+        return false;
+      },
+      false
+    );
+
+    el.addEventListener(
+      'dragend',
+      function(e) {
+        this.classList.remove('drag');
+        return false;
+      },
+      false
+    );
+  }
+});
+
+// drop
+app.directive('droppable', function() {
+  return {
+    scope: {
+      drop: '&',
+      bin: '='
+    },
+
+    link: function(scope, element) {
+      var el = element[0];
+
+      el.addEventListener(
+        'dragover',
+        function(e) {
+          e.dataTransfer.dropEffect = 'move';
+          if (e.preventDefault) e.preventDefault();
+          this.classList.add('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragenter',
+        function(e) {
+          this.classList.add('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'dragleave',
+        function(e) {
+          this.classList.remove('over');
+          return false;
+        },
+        false
+      );
+
+      el.addEventListener(
+        'drop',
+        function(e) {
+          if (e.stopPropagation) e.stopPropagation();
+
+          this.classList.remove('over');
+
+          var binId = this.id;
+          var item = document.getElementById(e.dataTransfer.getData('Text'));
+          this.appendChild(item);
+          scope.$apply(function(scope) {
+            var fn = scope.drop();
+            if ('undefined' !== typeof fn) {
+              fn(item.id, binId);
+            }
+          });
+
+          return false;
+        },
+        false
+      );
+    }
+  }
+});
+
+app.controller('uctrl', function($scope, $http, $location) {
+
+  $http.get('data/data.json')
+  .then(function(result){
+    $scope.page = result.data.page;
+    $scope.restroom = result.data.restroom;
+  },
+
+  function(error) {
+    console.log('There was an error :(');
+  });
+
+  $scope.location = $location;
+  $scope.$watch('location.search()', function() {
+    $scope.stage = ($location.search()).stage;
+
+    if ($scope.stage == undefined) {
+      $scope.stage = 0;
+    }
+
+  }, true);
+
+  $scope.changeTarget = function(name) {
+    $location.search('stage', name);
+  }
+
+  $scope.continue = false;
+
+  $scope.range = function(min, max, step){
+    step = step || 1;
+    var input = [];
+    for (var i = min; i <= max; i += step) input.push(i);
+    return input;
+  };
+
+  $scope.result = function(man, urinal) {
+
+    if (urinal == 'correct') {
+      $scope.page.message = 'checkmate';
+      $scope.continue = true;
+    } else {
+      $scope.page.message = 'wrong';
+      $scope.continue = false;
+    }
+
+  }
+
+  $scope.changeround = function() {
+    $scope.stage++;
+    location.reload();
+  }
+
+  $scope.endgame = function() {
+    $scope.page.message = 'game over';
+  }
+
+});
