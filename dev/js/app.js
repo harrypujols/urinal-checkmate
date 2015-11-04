45276a956a7570b4cc6a4f1b8060d921429df8bd
Harry Pujols <harry.pujols@pgmmb.com>
2015-11-04 14:28:07 -0500

    using vue.js

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,179 +1,34 @@
+var app = new Vue({
+  el: '#urinal-checkmate',
 
-var app = angular.module('urinal-chess', ['ngCookies']);
-
-// drag
-app.directive('draggable', function() {
-  return function(scope, element) {
-    var el = element[0];
-
-    el.draggable = true;
-
-    el.addEventListener(
-      'dragstart',
-      function(e) {
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('Text', this.id);
-        this.classList.add('drag');
-        return false;
-      },
-      false
-    );
-
-    el.addEventListener(
-      'dragend',
-      function(e) {
-        this.classList.remove('drag');
-        return false;
-      },
-      false
-    );
-  }
-});
-
-// drop
-app.directive('droppable', function() {
-  return {
-    scope: {
-      drop: '&',
-      bin: '='
-    },
-
-    link: function(scope, element) {
-      var el = element[0];
-
-      el.addEventListener(
-        'dragover',
-        function(e) {
-          e.dataTransfer.dropEffect = 'move';
-          if (e.preventDefault) e.preventDefault();
-          this.classList.add('over');
-          return false;
-        },
-        false
-      );
-
-      el.addEventListener(
-        'dragenter',
-        function(e) {
-          this.classList.add('over');
-          return false;
-        },
-        false
-      );
-
-      el.addEventListener(
-        'dragleave',
-        function(e) {
-          this.classList.remove('over');
-          return false;
-        },
-        false
-      );
-
-      el.addEventListener(
-        'drop',
-        function(e) {
-          if (e.stopPropagation) e.stopPropagation();
-
-          this.classList.remove('over');
-
-          var binId = this.id;
-          var item = document.getElementById(e.dataTransfer.getData('Text'));
-          this.appendChild(item);
-          scope.$apply(function(scope) {
-            var fn = scope.drop();
-            if ('undefined' !== typeof fn) {
-              fn(item.id, binId);
-            }
-          });
-
-          return false;
-        },
-        false
-      );
-    }
-  }
-});
-
-app.controller('uctrl', ['$scope', '$http', '$location', '$cookies', function($scope, $http, $location, $cookies) {
-
-  $http.get('data/data.json')
-  .then(function(result){
-    $scope.page = result.data.page;
-    $scope.restroom = result.data.restroom;
-    $scope.stage = $cookies.get('stage');
-
-    if ($scope.stage == undefined) {
-      $scope.stage = 0;
-
-    } else if ($scope.stage >= $scope.restroom.length) {
-      endgame();
-    }
-
-    if (Modernizr.touch) {
-      $scope.page.message = 'Play it on your desktop browser';
-    }
-
+  data: {
+    page: { message: 'loading...' },
+    colors: null
   },
 
-  function(error) {
-    console.log('There was an error :(');
-  });
+  created: function() {
+    this.update()
+  },
 
-  $scope.continue = false;
+  watch: {
+    'page.message': function (val, oldval) {
+      console.log('new: %s, old: %s', val, oldval)
+    }
+  },
 
-  $scope.range = function(min, max, step){
-    step = step || 1;
-    var input = [];
-    for (var i = min; i <= max; i += step) input.push(i);
-    return input;
-  };
+  methods: {
 
-  $scope.score = $cookies.get('score');
-
-  if ($scope.score == undefined) {
-    $scope.score = 0;
-    $cookies.put('score', $scope.score);
-  }
-
-
-  $scope.attempt = 0;
-
-  $scope.result = function(man, urinal) {
-    $scope.attempt++;
-
-    if (urinal == 'correct') {
-      $scope.page.message = 'checkmate';
-      $scope.continue = true;
-
-      if ($scope.attempt == 1) {
-        $scope.score++
-        $cookies.put('score', $scope.score, { expires: 0 });
+    update: function() {
+      var _this = this;
+      var request = new XMLHttpRequest()
+      request.open('GET', 'data.json')
+      request.onload = function () {
+        result = JSON.parse(request.responseText)
+        _this.page = result.page
+        _this.restroom = result.restroom
       }
-
-    } else {
-      $scope.page.message = 'wrong';
-      $scope.continue = false;
+      request.send();
     }
 
   }
-
-  $scope.nextround = function() {
-    $scope.stage++;
-    $cookies.put('stage', $scope.stage, { expires: 0 });
-    location.reload();
-  }
-
-  $scope.restart = function() {
-    $scope.endgame = false;
-    $cookies.put('stage', 0);
-    $cookies.put('score', 0);
-    location.reload();
-  }
-
-  var endgame = function() {
-    $scope.endgame = true;
-    $scope.page.message = 'Game Over';
-  }
-
-}]);
+});
